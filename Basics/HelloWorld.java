6a7cfbf2a3f531bd1e6de393163075b82c1deaa0
Prem Prakash <iamprem021@gmail.com>
2024-06-06 15:08:54 +0530

    basic

---
+++ Basics/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+    public static void main(String[] args){
+        System.out.println("Hello World, This is my first java program");
+
+        System.out.println(args[0]);
+    }
+}
